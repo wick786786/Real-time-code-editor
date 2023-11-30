7fd2af05e1cd5d71f1d19f2cebef4dec0d64ef7a
Satyam Yadav <86571155+wick786786@users.noreply.github.com>
2023-11-30 13:53:53 +0530

    Add files via upload

---
+++ build/precache-manifest.3a5b7f2326df864c7e5e49effb25d321.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6fcbd7572afeb0f9771b55e77ee61092",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1e11a3b9e8c54d33b872",
+    "url": "/static/css/2.19f4cc7c.chunk.css"
+  },
+  {
+    "revision": "937a7a682bb053f5220a",
+    "url": "/static/css/main.16ea483f.chunk.css"
+  },
+  {
+    "revision": "1e11a3b9e8c54d33b872",
+    "url": "/static/js/2.40c45940.chunk.js"
+  },
+  {
+    "revision": "d0775e084c9afc672e19",
+    "url": "/static/js/3.c114a520.chunk.js"
+  },
+  {
+    "revision": "937a7a682bb053f5220a",
+    "url": "/static/js/main.f809275e.chunk.js"
+  },
+  {
+    "revision": "2e86df9bf8258b67fb80",
+    "url": "/static/js/runtime~main.efe30245.js"
+  }
+]);
